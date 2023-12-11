eac75bb8db4cf00e57baa79cf8a66af241ca7c91
Kamaqen <jmcapcha@gmail.com>
2023-12-11 13:22:43 -0500

    set favoriteNumber as public to deploy and see

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.8; // This is the way to define the compiler version the ID
 contract SimpleStorage {
     // boolean, uint int, address, bytes
     // bool hasFavoriteNumber = true;
-    uint256 favoriteNumber; // 256 is the default size for uints, but it is always best to specify!!
+    uint256 public favoriteNumber; // 256 is the default size for uints, but it is always best to specify!!
     // string favoriteNumberInText = "Six";
     // int256 favoriteInt = -6;
     // address abAddress = 0xF4FaCa935238f1F12ce0E5499a567946b8556A0B;
