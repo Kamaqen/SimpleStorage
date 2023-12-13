9335a9b8117323187af9883d303b7f06e7685410
Kamaqen <jmcapcha@gmail.com>
2023-12-13 00:06:53 -0500

    add contract b

--- contractsAB.sol
+++ contractsAB.sol
@@ -6,4 +6,15 @@ contract A {
     constructor(address b) {
         bAddress = b;
     }
+
+    function callHello() external view returns(string memory){
+        B b = B(bAddress); // explicit conversion from address to contract type
+        return b.sayHello();
+    }
+}
+contract B {
+    string greeting = "hello world";
+    function sayHello() external view returns(string memory){
+        return greeting;
+    }
 }
