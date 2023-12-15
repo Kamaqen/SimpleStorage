43bb54660925bd35390355c0f055494c36f08bf5
Kamaqen <jmcapcha@gmail.com>
2023-12-15 17:16:14 -0500

    refactor sfstore and sfget

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -14,12 +14,10 @@ contract StorageFactory {
     function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public  {
         // Address -- We need this and also the ABI in order to interact with a contract.
         // ABI: Application Binary Interface
-        SimpleStorage simpleStorage = SimpleStorage(simpleStorageArray[_simpleStorageIndex]);
-        simpleStorage.store(_simpleStorageNumber);
+        SimpleStorage(simpleStorageArray[_simpleStorageIndex]).store(_simpleStorageNumber);
     }
 
     function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
-        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
-        return simpleStorage.retrieve();
+       return simpleStorageArray[_simpleStorageIndex].retrieve();
     }
 }
