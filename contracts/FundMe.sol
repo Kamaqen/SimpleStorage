bee2d799e9fc752702397813b79133b98ce9ecf6
Kamaqen <jmcapcha@gmail.com>
2023-12-18 15:31:06 -0500

    add fundme contract

---
+++ contracts/FundMe.sol
@@ -0,0 +1,21 @@
+// This contract will:
+// Get funds from users
+// Withdraw funds
+// Set a minimum funding value in USD
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract FundMe {
+    function fund() public payable {
+        // Contract addresses can hold funds, just like wallets!
+        // We need to be able to set a min func amount in USD
+        // 1. How do we send ETH to this contract?
+        require(msg.value > 1e18, "Didn't send enough!"); // 1e18 == 1 * 10 ** 18 == 1000000000000000000
+
+        // What is reverting?
+        // To undo any action from before and send the remaining gas back.
+    }
+
+    // function withdraw(){}
+}
