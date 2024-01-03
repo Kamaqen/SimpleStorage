62ccb1a3618f357af34a074b9d21f697f3f0b18e
Kamaqen <jmcapcha@gmail.com>
2024-01-02 21:32:56 -0500

    implement priceconverter to refactor functions

---
+++ PriceConverter.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    function getPrice() public view returns(uint256) {
+        // Requirements (because we are interacting with data outside our project): 
+        // ABI 
+        // interacting contract Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int price,,,) = priceFeed.latestRoundData();
+        // ETH price in terms of USD
+        // There are 8 decimal places associated with this price feed, eg:_ 300000000000 = 3000.0
+        return uint256(price * 1e10);
+    }
+
+    function getVersion() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+}
--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -6,8 +6,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-
 contract FundMe {
     uint256 public minimumUsd = 50 * 1e18;
 
@@ -28,28 +26,6 @@ contract FundMe {
         // To undo any action from before and send the remaining gas back.
         // The require keyword defines a condition that needs to be met in order for the transaction to go through.
         // Otherwise, the transaction reverts.
-    }
-
-    function getPrice() public view returns(uint256) {
-        // Requirements (because we are interacting with data outside our project): 
-        // ABI 
-        // interacting contract Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int price,,,) = priceFeed.latestRoundData();
-        // ETH price in terms of USD
-        // There are 8 decimal places associated with this price feed, eg:_ 300000000000 = 3000.0
-        return uint256(price * 1e10);
-    }
-
-    function getVersion() public view returns(uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        return priceFeed.version();
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
     }
     
     // function withdraw(){}
