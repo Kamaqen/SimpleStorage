2f5ef9f99e90b30425094278c09f7a445c2be350
Kamaqen <jmcapcha@gmail.com>
2024-01-02 21:41:26 -0500

    add import code to fundme

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -6,7 +6,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
+import "./PriceConverter.sol";
+
 contract FundMe {
+    using PriceConverter for uint256;
+
     uint256 public minimumUsd = 50 * 1e18;
 
     address[] public funders;
@@ -16,7 +20,7 @@ contract FundMe {
         // Contract addresses can hold funds, just like wallets!
         // We need to be able to set a min func amount in USD
         // 1. How do we send ETH to this contract?
-        require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough!"); // 1e18 == 1 * 10 ** 18 == 1000000000000000000
+        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough!"); // 1e18 == 1 * 10 ** 18 == 1000000000000000000
         // 18 decimals
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
