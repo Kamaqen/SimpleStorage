5f9e559d8cd37a0663a71affa2c1142a159c0c7a
Kamaqen <jmcapcha@gmail.com>
2024-01-02 22:34:12 -0500

    add withdraw funds functions in 3 ways

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -39,5 +39,19 @@ contract FundMe {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
+
+        // resetting the array
+        funders = new address[](0);
+
+        // actually withdraw the funds
+
+        // transfer
+        /* payable(msg.sender).transfer(address(this).balance); */
+        //send
+        /* bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        require(sendSuccess, "Send failed."); */
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed.");
     }
 }
